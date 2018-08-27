7eefba9f9e96e68e3eab4db678066789a80c78b9
richard <kmshi_array@yahoo.com>
2018-08-26 20:09:34 -0700

    add eureka client code

--- server/server.js
+++ server/server.js
@@ -16,6 +16,65 @@ var sslConfig = require('./ssl-config');
 var app = module.exports = loopback();
 const isSSLEnabled = process.env.HTTPS_ENABLED=="true"?true:false;
 
+const isEurekaEnabled = process.env.EUREKA_ENABLED=="true"?true:false;
+
+function startEurekaClient(){
+  const app_name = process.env.EUREKA_CLIENT_APPLICATION_NAME||require('../package.json').name;
+  const ip_address = process.env.EUREKA_CLIENT_EXTERNAL_IP || '127.0.0.1';
+  const host_name = process.env.EUREKA_CLIENT_EXTERNAL_HOSTNAME || 'localhost';
+  const eureka_urls = process.env.EUREKA_SERVICE_URL ||"http://localhost:8700/eureka/apps";
+  const app_port = process.env.EUREKA_CLIENT_EXTERNAL_PORT || app.get('port');
+  const container_id = process.env.HOSTNAME || '408daec6d2c8';
+  
+  const Eureka = require('eureka-js-client').Eureka;
+  const client = new Eureka({
+    instance: { 
+      instanceId: container_id +':'+app_name+':'+app_port,
+      app: app_name,
+      hostName: host_name,
+      ipAddr: ip_address,
+      healthCheckUrl: 'http://'+host_name+':'+app_port+'/health',
+      secureHealthCheckUrl: 'https://'+host_name+':'+app_port+'/health',
+      statusPageUrl: (isSSLEnabled? 'https://' : 'http://')+host_name+':'+app_port,
+      port: {
+        '$': app_port,
+        '@enabled': isSSLEnabled?'false':'true',
+      },
+      securePort:{
+        '$': app_port,
+        '@enabled': isSSLEnabled?'true':'false',
+      },
+      nonSecurePortEnabled: !isSSLEnabled,
+      securePortEnabled: isSSLEnabled,
+      vipAddress: app_name,
+      dataCenterInfo: {
+        '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+        name: 'MyOwn',
+      }
+    },
+    requestMiddleware: (requestOpts, done) => {
+      requestOpts.agentOptions = {
+        ca: fs.readFileSync(path.join(__dirname, './private/kujianet.pem'))
+      };
+      done(requestOpts);
+    },
+    eureka: {
+      preferIpAddress:false,
+      ssl:isSSLEnabled,
+      serviceUrls: {
+        default: eureka_urls.split(',')
+      }
+    },
+    // eureka: {
+    //   host: 'localhost',
+    //   port: 8700,
+    //   servicePath: '/eureka/apps/'
+    // }
+  });
+  client.start(err=>console.log('eureka register:',err));
+}
+
+
 app.use(cookieParser());
 
 app.start = function() {
@@ -51,4 +110,6 @@ boot(app, __dirname, function(err) {
   // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
+
+  if (isEurekaEnabled) startEurekaClient();
 });
