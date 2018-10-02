55c6129715555c1ff4ee4e4566c33ff6b67e0f95
kmshi_array <kmshi_array@yahoo.com>
2018-10-02 23:11:07 +0800

    add recognize helper api

--- server/models/helper.ts
+++ server/models/helper.ts
@@ -1,10 +1,17 @@
 import {resolve,reject} from "../lib/utils";
+import { CustomError } from "../lib/customError";
 var Nightmare = require('nightmare');
 const Xvfb = require('xvfb');
 const uuidv5 = require('uuid/v5');
 var Url = require('url');
-var Path = require('path');
-var fs = require('fs');
+const Busboy = require('busboy');
+import { ReadStream } from "fs";
+const AipSpeechClient = require("baidu-aip-sdk").speech;
+var client = new AipSpeechClient(process.env.BAIDU_AIP_APP_ID, process.env.BAIDU_AIP_API_KEY, process.env.BAIDU_AIP_SECRET_KEY);
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+var shelljs = require('shelljs');
 
 const qiniu = require("qiniu");
 var mac = new qiniu.auth.digest.Mac(process.env.QINIU_AK, process.env.QINIU_SK);
@@ -93,7 +100,7 @@ module.exports = function (Helper: any) {
     });
 
     Helper.copyRemoteFile = function(url:string,cb:Function){
-        let ext = Path.extname(Url.parse(url).pathname);
+        let ext = path.extname(Url.parse(url).pathname);
         if (ext=='') ext = '.jpg';
         let key = uuidv5(url,uuidv5.URL) + ext;
         //key = 'headimgs/'+key;//bind key with prefix
@@ -133,8 +140,8 @@ module.exports = function (Helper: any) {
         ]
     });
 
-    Helper.uptoken = function (key:string,cb:Function) {
-        if (!key) return reject('missed parameter key',cb);
+    Helper.uploadToken = function (key:string,cb:Function) {
+        if (!key) return cb('missed parameter key');
         let thumbUrl = qiniu.util.encodedEntry(process.env.QINIU_BUCKET,key+'-thumbnail');
         let options = {
             scope: process.env.QINIU_BUCKET+":"+key,
@@ -147,16 +154,112 @@ module.exports = function (Helper: any) {
           
         let putPolicy = new qiniu.rs.PutPolicy(options);
         let token = putPolicy.uploadToken(mac);
-        return resolve({uptoken:token, domain:process.env.QINIU_DOMAIN},cb);
+        cb(null,token,process.env.QINIU_DOMAIN+'/'+key,thumbUrl);
     }
 
-    Helper.remoteMethod('uptoken', {
+    Helper.remoteMethod('uploadToken', {
         accepts: [
             {arg: 'key',type: 'string',required:true}
         ],
-        http: {path: '/uptoken', verb: 'get'},
+        http: {path: '/uploadToken', verb: 'get'},
         returns: [
-            { arg: 'info', type: 'object',root:true}
+            { arg: 'token', type: 'string'},
+            { arg: 'picUrl', type: 'string'},
+            { arg: 'thumbUrl', type: 'string'}
         ]
+    });
+
+    Helper.recognize = function (ctx:any,cb:Function) {
+        const busboy = new Busboy({
+            headers: ctx.req.headers
+        });
+        let filePaths = new Array<string>();
+        let props:any = {lan:'zh'};
+        busboy.on('file', (fieldname:string, file:ReadStream, filename:string, encoding:string, mimetype:string)=> {
+            let key = path.basename(filename);
+            let saveTo = path.join(os.tmpdir(), key);
+            filePaths.push(saveTo);
+            file.pipe(fs.createWriteStream(saveTo));
+
+            var putPolicy = new qiniu.rs.PutPolicy({
+                scope: process.env.QINIU_BUCKET + ":" + 'voices/' + key
+            });
+            let uploadToken = putPolicy.uploadToken(mac);
+            formUploader.putStream(uploadToken, 'voices/' + key, file, putExtra, (err:any,
+                body:any, resp:any) => {
+                if (err) {
+                    console.error('qiniu.putStream.err',err);
+                    return;
+                }
+                console.log('qiniu.putStream.' + resp.statusCode,body);
+                // var bucketManager = new qiniu.rs.BucketManager(mac, config);
+                // bucketManager.deleteAfterDays(process.env.QINIU_BUCKET, key, 1,()=>{});//only keep 1 day
+            });
+        });
+        busboy.on('field', (fieldname:string, val:string) => {
+            props[fieldname]=val;
+        });
+        busboy.on('finish',()=>{
+            let localFile = filePaths[0];
+            let dotext = path.extname(localFile);
+            if (dotext=='.silk'){
+                //TODO:check how to decoder to 16000 pcm directly?
+                shelljs.exec('decoder '+localFile+' '+localFile.replace(dotext,'.pcm'),(code:string,stdout:string,stderr:string)=>{
+                    console.log('decoder '+localFile, +code==0?code:code + '...'+stdout+'...'+stderr);
+                    shelljs.rm(localFile);//silk
+                    localFile = localFile.replace(dotext,'.pcm');
+                    dotext = '.pcm';
+                    if (+code!=0) return cb(new CustomError(stderr,CustomError.errorCodes.BAIDU_AIP_ERROR));
+
+                    //TODO:check the above pcm file can work without converting to wav
+                    shelljs.exec('ffmpeg -i '+localFile+' -f wav -ar 16000 -ac 1 '+localFile.replace(dotext,'.wav'),(code:string,stdout:string,stderr:string)=>{
+                        console.log('ffmpeg -i '+localFile, +code==0?code:code + '...'+stdout+'...'+stderr);
+                        shelljs.rm(localFile);//pcm
+                        localFile = localFile.replace(dotext,'.wav');
+                        if (+code!=0) return cb(new CustomError(stderr,CustomError.errorCodes.BAIDU_AIP_ERROR));
+
+                        let voice = fs.readFileSync(localFile);
+                        let voiceBuffer = new Buffer(voice);
+                        shelljs.rm(localFile);//wav
+                        client.recognize(voiceBuffer, 'wav', 16000, {lan: props.lan}).then((res:any) =>{
+                            console.log('recognize:'+ props.lan,res);
+                            if(res.err_no) return cb(new CustomError(res.err_msg,CustomError.errorCodes.BAIDU_AIP_ERROR));
+                            cb(null,res.result,process.env.QINIU_DOMAIN+'/voices/' +path.basename(filePaths[0]));
+                        }).catch(cb);
+                    });
+                });
+            }else{
+                //TODO:check if convert to pcm directly:ffmpeg -i input.flv -f s16le -acodec pcm_s16le output.pcm
+                shelljs.exec('ffmpeg -i '+localFile+' -f wav -ar 16000 -ac 1 '+localFile.replace(dotext,'.wav'),(code:string,stdout:string,stderr:string)=>{
+                    console.log('ffmpeg -i '+localFile, +code==0?code:code + '...'+stdout+'...'+stderr);
+                    shelljs.rm(localFile);//m4a or mp3
+                    localFile = localFile.replace(dotext,'.wav');
+                    if (+code!=0) return cb(new CustomError(stderr,CustomError.errorCodes.BAIDU_AIP_ERROR));
+
+                    let voice = fs.readFileSync(localFile);
+                    let voiceBuffer = new Buffer(voice);
+                    shelljs.rm(localFile);//wav
+                    client.recognize(voiceBuffer, 'wav', 16000, {lan: props.lan}).then((res:any) =>{
+                        console.log('recognize:'+ props.lan,res);
+                        if(res.err_no) return cb(new CustomError(res.err_msg,CustomError.errorCodes.BAIDU_AIP_ERROR));
+                        cb(null,res.result,process.env.QINIU_DOMAIN + '/voices/' + path.basename(filePaths[0]));
+                    }).catch(cb);
+                });
+            }
+                       
+        });
+        ctx.req.pipe(busboy);
+    };
+
+    Helper.remoteMethod('recognize', {
+        description: 'recognize a audio file by upload',
+        accepts: [
+            { arg: 'ctx', type: 'object', http: { source: 'context' } }
+        ],
+        returns: [
+            { arg: 'speech', type: ['string']},
+            { arg: 'voiceUrl', type: 'string'}
+        ],
+        http: { verb: 'post' }
     });
 }
